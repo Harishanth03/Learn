9476b51539a81fdb0755c10786379903f863ff2e
Harishanth03 <kandeepanharishanth@gmail.com>
2024-10-21 17:47:55 +0530

    Start of the React

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import { LearnComponent } from "./components/LearnComponent";
+
+function App() {
+  return (
+    <>
+      <div>Harishanth</div>
+      <LearnComponent/>
+    </>
+  );
+}
+
+export default App;
