e989fcb88ee7fef2f51ccb5732fd94d8fd62b6c6
Harishanth03 <kandeepanharishanth@gmail.com>
2024-10-25 19:46:46 +0530

    finished css for advice app

---
+++ vite-project/src/Exersice/Advice App/AdviceApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './AdviceApp.css'
+
+export const AdviceApp = () => {
+  return (
+    <div>
+        <h3>Advice App</h3>
+        <button>Get Advice</button>
+    </div>
+  )
+}
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Counter } from './Exersice/Counter'
+import { AdviceApp } from './Exersice/Advice App/AdviceApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
-    <Counter/>
+    <AdviceApp/>
     
   </StrictMode>,
 )
