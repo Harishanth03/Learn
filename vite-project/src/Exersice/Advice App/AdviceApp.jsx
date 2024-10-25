c61394e9ab997033b7766553659adfb306a08425
Harishanth03 <kandeepanharishanth@gmail.com>
2024-10-25 20:06:24 +0530

    advice App finished

--- vite-project/src/Exersice/Advice App/AdviceApp.jsx
+++ vite-project/src/Exersice/Advice App/AdviceApp.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import './AdviceApp.css'
 
+import { useState } from 'react'
+
 export const AdviceApp = () => {
+
+    const[advice ,  setAdvice] = useState("Hello this is an advice app");
+
+    async function getAdvice()
+    {
+        const response = await fetch("https://api.adviceslip.com/advice");
+
+        const jsonResponse = await response.json();
+
+        console.log(jsonResponse)
+
+        setAdvice(jsonResponse.slip.advice)
+    }
+
+    useEffect(function(){
+        getAdvice();
+    }, []);
+
   return (
+
     <div>
-        <h3>Advice App</h3>
-        <button>Get Advice</button>
+
+        <h3>{advice}</h3>
+
+        <button onClick={getAdvice}>Get Advice</button>
+
     </div>
+
   )
+
 }
