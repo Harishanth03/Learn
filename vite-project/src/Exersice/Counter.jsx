932beadf953e4098650df3deb0ba7a295fe2384f
Harishanth03 <kandeepanharishanth@gmail.com>
2024-10-25 19:37:01 +0530

    button click increate finished

--- vite-project/src/Exersice/Counter.jsx
+++ vite-project/src/Exersice/Counter.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import './Counter.css'
+
+import { useState } from 'react'
+
 export const Counter = () => {
+
+  const [count , setCount] = useState(0);
+
   return (
 
-    <div>Counter</div>
+    <>
+      <div className='container'>
+
+        <button onClick={ () => alert("hi")}>Increase : {count} </button>
+        <button onClick={ () => setCount(count - 1)}>Decrease : {count}</button>
+
+      </div>
+    </>
     
   )
 }
